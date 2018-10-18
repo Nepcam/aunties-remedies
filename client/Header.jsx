a69f8e4d95233e0c36d69297f35ef7c576126dc5
Nepcam <cameron.nepe@gmail.com>
2018-10-19 09:37:14 +1300

    working on components

---
+++ client/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className='header'>
+                <h1>Aunties Remedies</h1>
+            </div>
+        )
+    }
+}
+
+export default Header 
