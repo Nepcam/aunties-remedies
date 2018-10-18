793a5a9ad8ae3674ccd0e152e6b5f76ca5188360
Nepcam <cameron.nepe@gmail.com>
2018-10-19 12:42:24 +1300

    working on components

--- client/App.jsx
+++ client/App.jsx
@@ -1,2 +1,24 @@
 import React from 'react'
 
+import Header from './Header'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+
+        }
+    }
+    render() {
+        return (
+            <div className='app'>
+                <Header />
+            </div>
+        )
+    }
+
+}
+
+export default App
---
+++ client/Listing.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Listing extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+
+    }
+    render() {
+        return (
+            <div className='listing'>
+                <p>Welcome to Aunties Remedies.</p>
+
+                
+            </div>
+        )
+    }
+}
+
+export default Listing 
